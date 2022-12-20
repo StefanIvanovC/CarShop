62d90e1bf327e331bd601b860971cc0d2f9efb23
admin <stefanivanovc@gmail.com>
2022-12-20 18:35:25 +0200

    Add xUnit project for tests

---
+++ Tests/CarShop.Test/CarYearMaxValueAtributeTest.cs
@@ -0,0 +1,22 @@
+﻿using CarShop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShop.Test
+{
+    public class CarYearMaxValueAtributeTest
+    {
+        [Fact]
+        public void YearCarTest()
+        {
+            var car = new Car();
+
+            car.Year = 2023;
+
+        }
+
+    }
+}
