cfcee68772f7e4087ac5bcdc7ab510ce00adf32f
admin <stefanivanovc@gmail.com>
2022-12-27 15:50:13 +0200

    Add Contact form in new view, add new controller

---
+++ Web/CarShop.Web/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿namespace CarShop.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ContactController : Controller
+    {
+        public IActionResult Contact()
+        {
+            return this.View();
+        }
+    }
+}
--- Web/CarShop.Web/Controllers/DeveloperController.cs
+++ Web/CarShop.Web/Controllers/DeveloperController.cs
@@ -2,7 +2,7 @@
 {
     using Microsoft.AspNetCore.Mvc;
 
-    public class Developer : Controller
+    public class DeveloperController : Controller
     {
         public IActionResult Info()
         {
