e1321d570eef6f3ba56b7027a254a557a8ed847f
admin <stefanivanovc@gmail.com>
2022-12-27 16:19:03 +0200

    Add inputViewModel for Contact form

---
+++ Web/CarShop.Web.ViewModels/Contacts/ContactInputFormModel.cs
@@ -0,0 +1,25 @@
+﻿namespace CarShop.Web.ViewModels.Contacts
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class ContactInputFormModel
+    {
+        [Required]
+        [MinLength(5)]
+        [MaxLength(15)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"08[789]\d{7}", ErrorMessage = "This phone number is fake")]
+        public string TelephoneNumber { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        [MaxLength(500)]
+        public string Message { get; set; }
+    }
+}
