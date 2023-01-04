b2bd46ca67a4608e7e44977c412313ef406aca3e
admin <stefanivanovc@gmail.com>
2023-01-04 11:41:24 +0200

    Add input form for items in shop. Add input model and interface in services

---
+++ Services/CarShop.Services.Data/IShopService.cs
@@ -0,0 +1,11 @@
+﻿namespace CarShop.Services.Data
+{
+    using System.Threading.Tasks;
+
+    using CarShop.Web.ViewModels.Shop;
+
+    public interface IShopService
+    {
+        Task AddAsync(AddItemShopInputModel input, string userId, string imagePath);
+    }
+}
---
+++ Web/CarShop.Web.ViewModels/Shop/AddItemShopInputModel.cs
@@ -0,0 +1,26 @@
+﻿namespace CarShop.Web.ViewModels.Shop
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Microsoft.AspNetCore.Http;
+
+    public class AddItemShopInputModel
+    {
+        [Required]
+        [MaxLength(50)]
+        [MinLength(5)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [MinLength(20)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range(0, 10)]
+        public int Price { get; set; }
+
+        [Required]
+        public IFormFile Image { get; set; }
+    }
+}
